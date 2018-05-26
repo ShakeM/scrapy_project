def17909dc97e05d53efa97eeb035f2960629277
Jonathan <527147029@qq.com>
2018-05-27 00:36:21 +0800

    Upload the project

---
+++ run_index.py
@@ -0,0 +1,24 @@
+rom scrapy_project.util import verify
+import yagmail
+from scrapy_project.spiders.baidu_index import BaiduIndexSpider
+from scrapy_project.spiders import baidu_index
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy_project.spiders.stocks import StockSpider
+
+if __name__ == '__main__':
+    b = baidu_index.BaiduIndexSpider()
+    cookies = b.cookies
+
+    if verify.bdusses(cookies):
+        process = CrawlerProcess(get_project_settings())
+        process.crawl(StockSpider)
+        process.crawl(BaiduIndexSpider)
+        process.start()
+    else:
+        yag = yagmail.SMTP('54jsy@163.com', '56304931a', 'smtp.163.com')
+        yag.send('18616020643@163.com', 'Cookie Fail')
+
+    if not verify.bdusses(cookies):
+        yag = yagmail.SMTP('54jsy@163.com', '56304931a', 'smtp.163.com')
+        yag.send('18616020643@163.com', 'Cookie Fail')
