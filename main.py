02fe6fc0fb7dc42cc6c5ada04c8d242ff2963620
Jonathan <527147029@qq.com>
2018-05-26 21:54:45 +0800

    Upload the project

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from scrapy import cmdline
 from scrapy_project.util import verify
 import yagmail
-import os
-import re
-from functools import reduce
+from scrapy_project.spiders.baidu_index import BaiduIndexSpider
+from scrapy_project.spiders.stocks import StockSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 from scrapy_project.spiders import baidu_index
+
+
+def crawl(spider):
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(spider)
+    process.start()
+
 
 if __name__ == '__main__':
     b = baidu_index.BaiduIndexSpider()
     cookies = b.cookies
 
     if verify.bdusses(cookies):
-        os.system('scrapy crawl stock_spider')
-        os.system('scrapy crawl baidu_index')
+        crawl(StockSpider)
+        crawl(BaiduIndexSpider)
         pass
     else:
         yag = yagmail.SMTP('54jsy@163.com', '56304931a', 'smtp.163.com')
-        yag.send('18616020643@163.com', 'Cookie失败')
+        yag.send('18616020643@163.com', 'Cookie Fail')
 
     if not verify.bdusses(cookies):
         yag = yagmail.SMTP('54jsy@163.com', '56304931a', 'smtp.163.com')
-        yag.send('18616020643@163.com', 'Cookie失败')
+        yag.send('18616020643@163.com', 'Cookie Fail')
